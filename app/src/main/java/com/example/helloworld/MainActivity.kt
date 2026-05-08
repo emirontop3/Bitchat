b83c3712b254a0026c652d9bf1ebf8859e885977
emirontop3 <ozl.kpz@gmail.com>
2026-05-08 18:28:59 +0300

    Create MainActivity.kt

---
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.helloworld
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val text = TextView(this)
+        text.text = "Hello World"
+        text.textSize = 24f
+
+        setContentView(text)
+    }
+}
